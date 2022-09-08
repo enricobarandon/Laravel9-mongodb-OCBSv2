b2b58fbc5f77fe936e9d6c871d7eb5124f6bed66
Enrico <enricobarandon@gmail.com>
2022-09-08 20:47:53 +0800

    debugbar

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Jenssegers\Mongodb\MongodbServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
